8ab628cac8b5e67623af8dbd8e99a261d184a89c
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-18 19:40:34 +0000

    ..

--- includes/senior-design-layout.php
+++ includes/senior-design-layout.php
@@ -7,6 +7,37 @@ function senior_design_display() {
         'sort_order' => 'asc'
     ) );
 
+    if (!empty($post_list)) {
+            
+        echo '<div class="row mb-5">';
+        foreach ($post_list as $post) {
+            setup_postdata($post);
+            $permalink = get_permalink($post);
+            $featured_image = get_the_post_thumbnail($post->ID, 'medium');
+            $job_title = get_field('person_jobtitle', $post->ID);
+
+            echo '<div class="card-box col-lg-2 col-md-3 col-sm-4 col-6">';
+            echo '<div class="card custom-card">';
+            echo '<a href="' . $permalink . '">';
+            if (!empty($featured_image)) {
+                echo $featured_image;
+            }
+            echo '<div class="card-body">';
+            echo '<h5 class="card-title">' . get_the_title($post) . '</h5>';
+            if (!empty($job_title)) {
+                echo '<div class="job-title"><i>' . esc_html($job_title) . '</i></div>';
+            }
+            echo '</div>';
+            echo '</a>';
+            echo '</div>';
+            echo '</div>';
+        }
+        echo '</div>';
+        wp_reset_postdata();
+    } else {
+        echo 'No posts found.';
+    }
+
     $posts = array();
 
     foreach ( $post_list as $post ) {
