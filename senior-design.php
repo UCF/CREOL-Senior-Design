d7c24e68e168c47fdb7e66570cf91590b62d3972
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-18 19:11:49 +0000

    Test layout

--- senior-design.php
+++ senior-design.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: {{My Project}}
+Plugin Name: Senior Design
 Description: {{Short project description here}}
 Version: 0.0.0
 Author: UCF Web Communications
 License: GPL3
-GitHub Plugin URI: UCF/{{My-Project}}
+GitHub Plugin URI: UCF/CREOL-Senior-Design
 */
 
 if ( ! defined( 'WPINC' ) ) {
