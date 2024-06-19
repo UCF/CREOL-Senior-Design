2c71b3884b5416a06e6e5ba91c903e0967251c6d
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-19 18:54:38 +0000

    .

--- includes/senior-design-layout.php
+++ includes/senior-design-layout.php
@@ -5,7 +5,7 @@ function senior_design_display() {
         'posts_per_page' => -1,
         'post_type'      => 'post',
         'post_status'    => 'publish',
-        'category_name'  => 'Fall 2019',
+        'category_name'  => 'Fall 2020',
     ));
 
     if (!empty($post_list)) {
