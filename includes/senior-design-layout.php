55e5a85c894460c7c9fdededa8332f1fcd89953f
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-19 19:16:20 +0000

    ..

--- includes/senior-design-layout.php
+++ includes/senior-design-layout.php
@@ -1,12 +1,19 @@
 <?php
 
 function senior_design_display() {
-    $post_list = get_posts(array(
+    // Determine the current page
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+    $args = array(
         'posts_per_page' => 5,
+        'paged'          => $paged,
         'post_type'      => 'post',
         'post_status'    => 'publish',
-        'cat'  => '319, 320, 321, 322, 323, 324, 325, 326, 337, 328, 329, 330',
-    ));
+        'cat'            => '319, 320, 321, 322, 323, 324, 325, 326, 337, 328, 329, 330'
+    );
+
+    $post_list = get_posts($args);
+    $total_posts = count(get_posts(array_merge($args, ['posts_per_page' => -1])));
 
     if (!empty($post_list)) {
         echo '<style>
@@ -27,21 +34,34 @@ function senior_design_display() {
             setup_postdata($post);
             $permalink = get_permalink($post);
             
-            
             echo '<div class="card-box col-12">';
             echo '<a href="' . $permalink . '">';
             echo '<div class="card custom-card">';
-            
             echo '<div class="card-body">';
             echo '<h5 class="card-title">' . get_the_title($post) . '</h5>';
             echo '</div>';
-            
             echo '</div>';
             echo '</a>';
             echo '</div>';
-            
         }
         echo '</div>';
+        
+        // Pagination
+        $total_pages = ceil($total_posts / 5);
+        if ($total_pages > 1){
+            $current_page = max(1, get_query_var('paged'));
+            echo '<div class="pagination">';
+            echo paginate_links(array(
+                'base'      => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+                'format'    => '?paged=%#%',
+                'current'   => $current_page,
+                'total'     => $total_pages,
+                'prev_text' => __('&laquo; Prev'),
+                'next_text' => __('Next &raquo;'),
+            ));
+            echo '</div>';
+        }
+
         wp_reset_postdata();
     } else {
         echo 'No posts found.';
