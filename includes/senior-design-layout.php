123f9e6138a4082b47bb86fb4751757ee9072411
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-20 16:42:28 +0000

    ..

--- includes/senior-design-layout.php
+++ includes/senior-design-layout.php
@@ -1,10 +1,8 @@
 <?php
 
 function senior_design_display() {
-    // Determine the current page
     $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
-    // Retrieve the category and search term from the URL parameter
     $category = isset($_GET['category']) ? $_GET['category'] : '';
     $search_term = isset($_GET['search']) ? $_GET['search'] : '';
 
@@ -13,13 +11,13 @@ function senior_design_display() {
         'paged'          => $paged,
         'post_type'      => 'post',
         'post_status'    => 'publish',
-        's'              => $search_term  // Search parameter
+        's'              => $search_term
     );
 
     if (!empty($category)) {
         $args['cat'] = $category;
     } else {
-        $args['cat'] = '319, 320, 321, 322, 323, 324, 325, 326, 337, 328, 329, 330'; // Default categories
+        $args['cat'] = '319, 320, 321, 322, 323, 324, 325, 326, 337, 328, 329, 330';
     }
 
     $post_list = get_posts($args);
@@ -28,17 +26,17 @@ function senior_design_display() {
 
     if (!empty($post_list)) {
         echo '<style>
-                .custom-card {
-                    border-radius: 15px;
-                    box-shadow: 0 3px 6px rgba(0,0,0,0.1);
-                    margin-bottom: 20px;
-                    padding: 20px;
-                    transition: box-shadow 0.3s ease-in-out;
-                }
-                .custom-card:hover {
-                    box-shadow: 0 4px 8px rgba(0,0,0,0.3); 
-                }
-              </style>';
+            .custom-card {
+                border-radius: 15px;
+                box-shadow: 0 3px 6px rgba(0,0,0,0.1);
+                margin-bottom: 20px;
+                padding: 20px;
+                transition: box-shadow 0.3s ease-in-out;
+            }
+            .custom-card:hover {
+                box-shadow: 0 4px 8px rgba(0,0,0,0.3); 
+            }
+            </style>';
 
         echo '<div class="container mb-4" >
                 <div class="row">
@@ -47,7 +45,7 @@ function senior_design_display() {
                         <div class="form-group mr-4">
                             <select class="form-control" id="categorySelector" name="category" onchange="this.form.submit()" style="width: 100%;">
                                 <option value="">All Semesters</option>';
-                                $categories = get_categories(array('include' => '319, 320, 322, 323, 324, 325, 326, 337, 328, 329, 330'));
+                                $categories = get_categories(array('include' => '319, 320, 322, 323, 324, 325, 326, 327, 328, 329, 330'));
                                 foreach ($categories as $category_option) {
                                     $selected = ($category_option->term_id == $category) ? ' selected' : '';
                                     echo '<option value="' . esc_attr($category_option->term_id) . '"' . $selected . '>' . esc_html($category_option->name) . '</option>';
