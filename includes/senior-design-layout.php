f96117b2725671c7c17981752d2289288cfb2729
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-25 17:55:45 +0000

    .

--- includes/senior-design-layout.php
+++ includes/senior-design-layout.php
@@ -51,7 +51,7 @@ function senior_design_display() {
 
                         <div class="form-group mr-4">
                             <select class="form-control" id="categorySelector" name="category" style="width: 100%;">
-                                <option value="">All Semesters</option>';
+                                <option value="">All Semester</option>';
         // Fetch categories and populate dropdown
         $categories = get_categories(array('include' => '319, 320, 322, 323, 324, 325, 326, 327, 328, 329, 330'));
         foreach ($categories as $category_option) {
@@ -95,10 +95,10 @@ function senior_design_display() {
             echo '<nav aria-label="Page navigation">';
             echo '<ul class="pagination justify-content-center">';
 
-            $base_link = esc_url(remove_query_arg(['paged'], get_pagenum_link(1)));
+            $base_link = remove_query_arg(['paged'], get_pagenum_link(1));
             $current_page = max(1, get_query_var('paged'));
 
-            $link_with_params = add_query_arg(['category' => $category], $base_link);
+            $link_with_params = add_query_arg(['category' => $category, 'search' => $search_term], $base_link);
             
             if ($current_page > 1) {
                 echo '<li class="page-item"><a class="page-link" href="' . esc_url(add_query_arg(['paged' => $current_page - 1], $link_with_params)) . '" aria-label="Previous"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>';
