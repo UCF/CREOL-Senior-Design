2933f342cbe699ae594b64d12f535662ed084c19
Jonathan Connor <125600046+jconn0@users.noreply.github.com>
2024-06-25 16:26:47 +0000

    ..

--- includes/senior-design-layout.php
+++ includes/senior-design-layout.php
@@ -19,7 +19,7 @@ function senior_design_display() {
     if (!empty($category)) {
         $args['cat'] = $category;
     } else {
-        $args['cat'] = '319, 320, 322, 323, 324, 325, 326, 327, 328, 329, 330';
+        $args['cat'] = '319, 320, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341';
     }
 
     // Retrieve the list of posts based on the arguments
@@ -95,27 +95,26 @@ function senior_design_display() {
             echo '<nav aria-label="Page navigation">';
             echo '<ul class="pagination justify-content-center">';
 
-            $base_link = esc_url(get_pagenum_link(1));
+            $base_link = esc_url(remove_query_arg(['paged'], get_pagenum_link(1)));
             $current_page = max(1, get_query_var('paged'));
 
             if ($current_page > 1) {
-                echo '<li class="page-item"><a class="page-link" href="' . add_query_arg('paged', $current_page - 1, $base_link) . '" aria-label="Previous"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>';
+                echo '<li class="page-item"><a class="page-link" href="' . esc_url(add_query_arg(['paged' => $current_page - 1], $base_link)) . '" aria-label="Previous"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>';
             }
 
             for ($i = 1; $i <= $total_pages; $i++) {
                 if ($i == $current_page) {
                     echo '<li class="page-item active"><a class="page-link" href="#">' . $i . '</a></li>';
                 } else {
-                    echo '<li class="page-item"><a class="page-link" href="' . add_query_arg('paged', $i, $base_link) . '">' . $i . '</a></li>';
+                    echo '<li class="page-item"><a class="page-link" href="' . esc_url(add_query_arg(['paged' => $i], $base_link)) . '">' . $i . '</a></li>';
                 }
             }
 
             if ($current_page < $total_pages) {
-                echo '<li class="page-item"><a class="page-link" href="' . add_query_arg('paged', $current_page + 1, $base_link) . '" aria-label="Next"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>';
+                echo '<li class="page-item"><a class="page-link" href="' . esc_url(add_query_arg(['paged' => $current_page + 1], $base_link)) . '" aria-label="Next"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>';
             }
 
-            echo '</ul>';
-            echo '</nav>';
+            echo '</ul></nav>';
         }
 
         wp_reset_postdata();
