61d75fd5b40070700182ac49c1c16db75eab244d
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-07-26 21:01:44 +0000

    ...

--- includes/revised-sd-plugin.php
+++ includes/revised-sd-plugin.php
@@ -111,9 +111,9 @@ function new_senior_design_display() {
         foreach ($post_list as $post) {
             setup_postdata($post);
             $permalink = get_permalink($post);
-            $short_report = get_field('field_short_report');
-            $long_report = get_field('field_long_report');
-            $presentation_slides = get_field('field_presentation_slides');
+            $short_report = get_field('short_report');
+            $long_report = get_field('long_report');
+            $presentation_slides = get_field('presentation_slides');
 
             echo '<div class="card-box col-12">';
             // echo '<a href="' . $permalink . '">';
@@ -121,9 +121,11 @@ function new_senior_design_display() {
             echo '<div class="card-body">';
             echo '<h5 class="card-title mb-1" style="margin-top: 2px;">' . get_the_title($post) . '</h5>';
             echo '<div class="project-reports">';
-                echo 'View: <a href="' . esc_url($short_report) . '">Short Report</a> | ';
+            if($short_report || $long_report || $presentation_slides) {
+                echo 'View: <a href="' . $short_report . '">8-Page Report</a> | ';
                 echo '<a href="' . $long_report . '">100-Page Report</a> | ';
                 echo '<a href="' . $presentation_slides . '">Presentation Slides</a> | ';
+            }
             echo '</div>';
             echo '</div>';
             echo '</div>';
