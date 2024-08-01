776b037d2982c3e05ffe4a0f482790f345a7b3e3
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-08-01 19:02:37 +0000

    Search bar patch v3

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -114,7 +114,7 @@ function sd_project_display($atts) {
             $base_link = esc_url_raw(remove_query_arg(['paged'], get_pagenum_link(1)));
             $current_page = max(1, get_query_var('paged'));
 
-            $link_with_params = esc_url_raw(add_query_arg(['semester' => $semester, 'search' => $search], $base_link));
+            $link_with_params = esc_url_raw(add_query_arg(['semester' => $semester], $base_link));
 
             if ($current_page > 1) {
                 echo '<li class="page-item"><a class="page-link" href="' . esc_url_raw(add_query_arg(['paged' => $current_page - 1], $link_with_params)) . '" aria-label="Previous"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>';
