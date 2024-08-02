4e4b12e28c55264e18dee5c958ceba9673d8b420
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-08-02 18:23:13 +0000

    Removed AJAX calls

--- senior-design-projects.php
+++ senior-design-projects.php
@@ -17,7 +17,6 @@ if (!defined('ABSPATH')) {
 include(plugin_dir_path(__FILE__) . 'includes/cpt.php');
 include(plugin_dir_path(__FILE__) . 'includes/taxonomy.php');
 include(plugin_dir_path(__FILE__) . 'includes/shortcode.php');
-include(plugin_dir_path(__FILE__) . 'includes/ajax-handler.php');
 
 // Register hooks
 register_activation_hook(__FILE__, 'sd_activate_plugin');
@@ -33,13 +32,5 @@ function sd_activate_plugin() {
 function sd_deactivate_plugin() {
     // Deactivation code here
     flush_rewrite_rules();
-}
-
-function sd_enqueue_scripts() {
-    wp_enqueue_script('sd-ajax', plugin_dir_url(__FILE__) . 'assets/js/sd-ajax.js', array('jquery'), null, true);
-
-    wp_localize_script('sd-ajax', 'sd_ajax_obj', array(
-        'ajax_url' => admin_url('admin-ajax.php'),
-    ));
 }
 add_action('wp_enqueue_scripts', 'sd_enqueue_scripts');
