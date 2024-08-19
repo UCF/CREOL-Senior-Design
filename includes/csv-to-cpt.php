f3134bf0459fdd9327c41fb23429120907f75014
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-08-19 18:08:24 +0000

    ...

--- includes/csv-to-cpt.php
+++ includes/csv-to-cpt.php
@@ -8,7 +8,7 @@
         add_action( "admin_notices", function() {
             echo "<div class='updated'>";
             echo "<p>";
-            echo "To import Senior Design Projects from the CSV, click the button to the right."; // TODO: Change to better explaination
+            echo "To insert the posts into the database, click the button to the right.";
             echo "<a class='button button-primary' style='margin:0.25em 1em' href='{$_SERVER["REQUEST_URI"]}&insert_sd_projects'>Insert Posts</a>";
             echo "</p>";
             echo "</div>";
