d6f5eeb64a5e7cf0a5bcf46a4b57854363bceb3e
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-08-19 17:55:12 +0000

    ...

--- includes/csv-to-cpt.php
+++ includes/csv-to-cpt.php
@@ -5,16 +5,24 @@
         ob_start();
 
         // This adds an action button to the Senior Design Projects page in WOrdPress
-        add_action('admin_notices', function() {
-            $screen = get_current_screen();
-            if ($screen->$post_type == 'sd_project' && $screen->base == 'edit') {
-                echo "<div class='updated'>";
-                echo "<p>";
-                echo "To import Senior Design Projects from the CSV, click the button to the right."; // TODO: Change to better explaination
-                echo "<a class='button button-primary' style='margin:0.25em 1em' href='{$_SERVER["REQUEST_URI"]}&insert_sd_projects'>Insert Posts</a>";
-                echo "</p>";
-                echo "</div>";
-            }
+        // add_action('admin_notices', function() {
+        //     $screen = get_current_screen();
+        //     if ($screen->$post_type == 'sd_project' && $screen->base == 'edit') {
+        //         echo "<div class='updated'>";
+        //         echo "<p>";
+        //         echo "To import Senior Design Projects from the CSV, click the button to the right."; // TODO: Change to better explaination
+        //         echo "<a class='button button-primary' style='margin:0.25em 1em' href='{$_SERVER["REQUEST_URI"]}&insert_sd_projects'>Insert Posts</a>";
+        //         echo "</p>";
+        //         echo "</div>";
+        //     }
+        // });
+        add_action( "admin_notices", function() {
+            echo "<div class='updated'>";
+            echo "<p>";
+            echo "To insert the posts into the database, click the button to the right.";
+            echo "<a class='button button-primary' style='margin:0.25em 1em' href='{$_SERVER["REQUEST_URI"]}&insert_sitepoint_posts'>Insert Posts</a>";
+            echo "</p>";
+            echo "</div>";
         });
 
         // This executes when the page is loaded
