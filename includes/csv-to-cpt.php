8bf42bd0531822500d8428b9fe461b62e4168835
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-08-19 18:17:53 +0000

    ...

--- includes/csv-to-cpt.php
+++ includes/csv-to-cpt.php
@@ -3,7 +3,7 @@
         // This adds an action button to the Senior Design Projects page in WOrdPress
         add_action('admin_notices', function() {
             $screen = get_current_screen();
-            if ($screen->$post_type == 'sd_project' && $screen->base == 'edit') {
+            if ($screen->post_type == 'sd_project' && $screen->base == 'edit') {
                 echo "<div class='updated'>";
                 echo "<p>";
                 echo "To import Senior Design Projects from the CSV, click the button to the right."; // TODO: Change to better explaination
