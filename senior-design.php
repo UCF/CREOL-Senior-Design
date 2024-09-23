d033faa804b67987f0e63270be49a39cb5d5e64a
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-09-23 19:42:25 +0000

    ...

--- senior-design.php
+++ senior-design.php
@@ -12,10 +12,10 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-require_once 'includes/senior-design-layout.php';
+// require_once 'includes/senior-design-layout.php';
 require_once 'includes/senior-design-v2.php';
 require_once 'includes/csv-to-cpt.php';
 
 
-add_shortcode( 'senior-design', 'senior_design_display');
+// add_shortcode( 'senior-design', 'senior_design_display');
 add_shortcode('sd_project_display', 'sd_project_display');
