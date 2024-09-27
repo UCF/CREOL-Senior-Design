08ac5c51f7dba49202bcb77cf3898746bb05c5c1
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-09-27 16:02:13 +0000

    ...

--- includes/senior-design-v2.php
+++ includes/senior-design-v2.php
@@ -57,15 +57,15 @@ function sd_project_display($atts) {
         );
     }
     
-    // Search filtering
+    // Meta query for contributors
     if ($search) {
         $args['meta_query'] = array(
             'relation' => 'OR',
             array(
                 'key' => 'project_contributors',
                 'value' => $search,
-                'compare' => 'LIKE'
-            )
+                'compare' => 'LIKE',
+            ),
         );
     }
     
