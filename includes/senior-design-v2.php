cfbadcb56f43edc01e7152148a5f1f484948304f
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-09-27 16:04:59 +0000

    ...

--- includes/senior-design-v2.php
+++ includes/senior-design-v2.php
@@ -59,6 +59,8 @@ function sd_project_display($atts) {
     
     $query = new WP_Query($args);
     
+    echo '<p>Search parameter: ' . esc_html($search) . '</p>';
+
     ob_start();
     echo '<style>
         .custom-card {
