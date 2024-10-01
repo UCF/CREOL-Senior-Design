f1f09720132cecc20951dd635e73e896c982bb0b
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-10-01 17:50:16 +0000

    Attempt 2 at select2 integration

--- senior-design.php
+++ senior-design.php
@@ -12,6 +12,18 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
+function my_plugin_enqueue_select2() {
+    // Enqueue Select2 CSS
+    wp_enqueue_style('select2-css', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css');
+
+    // Enqueue Select2 JS
+    wp_enqueue_script('select2-js', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', array('jquery'), null, true);
+
+    // Enqueue your custom script to initialize Select2
+    wp_enqueue_script('my-plugin-select2-init', plugin_dir_url(__FILE__) . 'assets/js/select2-init.js', array('select2-js'), null, true);
+}
+add_action('wp_enqueue_scripts', 'my_plugin_enqueue_select2');
+
 // require_once 'includes/senior-design-layout.php';
 require_once 'includes/senior-design-v2.php';
 require_once 'includes/csv-to-cpt.php';
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,6 @@
 //
 // Import third-party scripts
-//= require select2/dist/js/select2.full.min.js
-
-// Initialize Select2
-$(function() {
-    $('.select2').select2();
-});
+//
 
 // Require any third-party scripts here
 // see https://www.npmjs.com/package/gulp-include#include-directives
