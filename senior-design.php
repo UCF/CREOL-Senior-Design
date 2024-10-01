2b092f0e229b99979065735d1cfce6369632d23b
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-10-01 18:31:18 +0000

    ...

--- senior-design.php
+++ senior-design.php
@@ -18,25 +18,8 @@ function select2_test_enqueue_scripts() {
 
     // Enqueue Select2 JS
     wp_enqueue_script('select2-js', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', array('jquery'), null, true);
-
-    // Enqueue custom script to initialize Select2
-    wp_enqueue_script('select2-test-init', plugin_dir_url(__FILE__) . 'includes/select2-test-init.js', array('select2-js'), null, true);
 }
 add_action('wp_enqueue_scripts', 'select2_test_enqueue_scripts');
-
-function select2_test_shortcode() {
-    ob_start();
-    ?>
-    <!-- <div class="form-check"> -->
-        <select class="form-control" id="testSelect2" style="width: 100%;">
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-        </select>
-    <!-- </div> -->
-    <?php
-    return ob_get_clean();
-}
 
 // require_once 'includes/senior-design-layout.php';
 require_once 'includes/senior-design-v2.php';
@@ -45,4 +28,3 @@ require_once 'includes/csv-to-cpt.php';
 
 // add_shortcode( 'senior-design', 'senior_design_display');
 add_shortcode('sd_project_display', 'sd_project_display');
-// add_shortcode('select2_test', 'select2_test_shortcode');
