64758e858d8317c5f20f1e638a5aee0f10845e1c
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-10-01 18:17:08 +0000

    ...

--- includes/select2-test-init.js
+++ includes/select2-test-init.js
@@ -2,6 +2,7 @@ $(function() {
     $('#testSelect2').select2({
         placeholder: "Select an option",
         allowClear: true,
-        width: 'resolve'
+        width: 'resolve',
+        multiple: true,
     });
 });
