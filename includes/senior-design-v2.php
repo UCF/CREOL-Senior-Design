311aaca13061ee3493b6e37410389e3dd0214f95
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-10-02 14:14:48 +0000

    ...

--- includes/senior-design-v2.php
+++ includes/senior-design-v2.php
@@ -357,13 +357,7 @@ function sd_project_display($atts) {
         }
 
         $(function() {
-            $('#multiSemesterSelector').select2({
-                placeholder: {
-                    id: '1',
-                },
-                allowClear: true, // This adds an "X" button to clear the selection
-                width: 'resolve' // This makes the dropdown fit the width of its container
-            });
+            $('#multiSemesterSelector').select2();
         });
     });
     </script>
