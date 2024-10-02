6c8d137c4b3a128954f3f49d919459bd6c2e9994
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-10-02 13:52:01 +0000

    ...

--- includes/senior-design-v2.php
+++ includes/senior-design-v2.php
@@ -80,6 +80,46 @@ function sd_project_display($atts) {
             width: 100%;
             margin-top: 10px;
         }
+        /* Custom styles for Select2 */
+        .select2-container .select2-selection--single {
+            height: 40px;
+            border-radius: 4px;
+            border: 1px solid #ced4da;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__rendered {
+            line-height: 38px;
+            padding-left: 10px;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__arrow {
+            height: 38px;
+            right: 10px;
+        }
+
+        .select2-container--default .select2-selection--multiple {
+            min-height: 40px;
+            border-radius: 4px;
+            border: 1px solid #ced4da;
+        }
+
+        .select2-container--default .select2-selection--multiple .select2-selection__rendered {
+            padding: 5px;
+        }
+
+        .select2-container--default .select2-selection--multiple .select2-selection__choice {
+            background-color: #007bff;
+            border: 1px solid #007bff;
+            color: white;
+            padding: 3px 10px;
+            border-radius: 4px;
+            margin: 2px 5px 2px 0;
+        }
+
+        .select2-container--default .select2-selection--multiple .select2-selection__choice__remove {
+            color: white;
+            margin-right: 5px;
+        }
     </style>';
     
     // Display semester dropdown
