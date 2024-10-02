6ac44c5a1cfef3faf8b3687e7bd0cf3c1b98a7cd
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-10-02 14:03:53 +0000

    ...

--- includes/senior-design-v2.php
+++ includes/senior-design-v2.php
@@ -113,7 +113,7 @@ function sd_project_display($atts) {
     // Multi-select dropdown for semesters
     echo '          <div class="collapse" id="multiSemesterCollapse">';
     echo '              <label for="multiSemesterSelector">Select Semesters</label>';
-    echo '              <select class="form-control mb-4" id="multiSemesterSelector" name="selected_semesters[]" multiple="multiple" style="width: 100%;">';
+    echo '              <select class="form-control mb-4 multi-select" id="multiSemesterSelector" name="selected_semesters[]" multiple="multiple" style="width: 100%;">';
     foreach ($terms as $term) {
         $selected = in_array($term->slug, $selected_semesters) ? 'selected="selected"' : '';
         echo '                  <option value="' . esc_attr($term->slug) . '" ' . $selected . '>' . esc_html($term->name) . '</option>';
