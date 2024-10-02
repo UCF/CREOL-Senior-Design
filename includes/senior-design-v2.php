a42647f9710ba6567b51df4c958676b2e781263a
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-10-02 17:19:31 +0000

    ...

--- includes/senior-design-v2.php
+++ includes/senior-design-v2.php
@@ -357,7 +357,9 @@ function sd_project_display($atts) {
         }
 
         $(function() {
-            $('#multiSemesterSelector').select2();
+            $('#multiSemesterSelector').select2({
+                dropdownParent: $('body')
+            });
         });
     });
     </script>
